edd07688ff3817ae228333ed8484f94a90fcde7a
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-10-25 16:09:19 -0300

    create a basic route

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!!!"));
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Mini message board app! Listening on port ${PORT}`);
+});
